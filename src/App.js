c499dfcea950c3e987c8167aa2d9fd650a940dd6
Patrick Abbott <patrickmabbott@yahoo.com>
2021-01-01 14:43:31 -0800

    branch 001

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';  
+import './App.css';
+let recipient = "Orlando";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //recipient = recipient + "ly people"
+
+    alert(`Hello ${recipient}`);
+    console.log(`Hello ${recipient}`);
+  }
+
+  render() {
+    return <h1>Hello {recipient}!</h1>;
+  }
 }
 
 export default App;
